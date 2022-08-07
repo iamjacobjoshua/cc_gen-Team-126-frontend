d7d1680462d91cbc346e705aa18b7c8a17eb5a13
Avus <avuwake@gmail.com>
2022-08-07 18:31:03 +0100

    signup & login pages optimized

---
+++ script/main.js
@@ -0,0 +1,5 @@
+'use strict'
+//selecting UI components
+const mobileMenu = document.querySelector('#hmbg')
+
+mobileMenu.addEventListener('click', ()=> alert('Work in progress. login.html & signup.html are available'))
